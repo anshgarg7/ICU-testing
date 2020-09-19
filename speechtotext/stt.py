23c857b89be67d409fa0986670f1c669abad1ba1
Ansh Garg <anshgarg7@gmail.com>
2020-09-19 23:20:17 +0530

    fix

--- speechtotext/stt.py
+++ speechtotext/stt.py
@@ -8,12 +8,16 @@ m = sr.Microphone()
 print("-----------------START SPEAKING------------------")
 response=recognize_speech_from_mic(r, m)  # speak after running this line
 print(response)
-return_tuple = (response["transcription"],response["success"],response["error"])
+test = []
+test.append(response["transcription"])
+test.append(response["success"])
+test.append(response["error"])
+return_tuple = (test[0],test[1],test[2])
 connection = mysql.connector.connect(host='localhost',database='icu',user='root',password='')
 cursor = connection.cursor()
-recordTuple = response
+recordTuple = return_tuple
 print(recordTuple)
-cursor.execute("""INSERT INTO audiodata (transcript, success, error) VALUES (%s,%s,%s)""",recordTuple)
+cursor.execute("""INSERT INTO audiodata (transcript, success, error) VALUES (%s,%s,%s);""",recordTuple)
 connection.commit()
 print(cursor.rowcount, "Record inserted successfully into database")
 cursor.close()
